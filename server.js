a42b356dd677dad1c9c74bac11408c912fef7930
Stephon Treadwell <104699408+StephonT@users.noreply.github.com>
2022-07-24 22:46:17 -0400

    built database calls

--- server.js
+++ server.js
@@ -22,9 +22,38 @@ const db = mysql.createConnection(
   );
 
 //This method runs the SQL query and executes the callback with all the resulting rows that match the query.
-  db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-  })
+  // db.query(`SELECT * FROM candidates`, (err, rows) => {
+  //   console.log(rows);
+  // })
+
+  // GET a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
+
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) 
+              VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(result);
+});
 
   // Default response for any other request (Not Found)
 app.use((req, res) => {
